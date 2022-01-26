0cf8cc443129b374f03e5a88b5985a044db16c1a
lovebird <lovebirdsx@gmail.com>
2022-01-27 00:13:01 +0800

    使用循环来渲染出棋盘的格子

--- src/index.js
+++ src/index.js
@@ -22,25 +22,15 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    const rows = [];
+    for (let r = 0; r < 3; r++) {
+      const cols = [];
+      for (let c = 0; c < 3; c++) {
+        cols.push(this.renderSquare(c + r * 3));
+      }
+      rows.push(<div className='board-row'>{cols}</div>);
+    }
+    return (<div>{rows}</div>);
   }
 }
 
